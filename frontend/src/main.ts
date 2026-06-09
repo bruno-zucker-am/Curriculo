65d0332d2f6cfd0af9caa5c15ac295df369175b6
Bruno Zucker <bruno.zucker.am@gmail.com>
2026-06-09 16:50:39 -0400

    Add files via upload

---
+++ frontend/src/main.ts
@@ -0,0 +1,25 @@
+import 'zone.js';
+import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
+  ],
+});
